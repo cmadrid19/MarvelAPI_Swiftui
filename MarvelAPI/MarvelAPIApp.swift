f785ee1cfda71aba1b6b4fbb11e80c5c209ea08b
Max <maxim.macari.dam@gmail.com>
2021-03-15 16:11:15 +0100

    Initial Commit

---
+++ MarvelAPI/MarvelAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAPIApp.swift
+//  MarvelAPI
+//
+//  Created by Maxim Macari on 15/3/21.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
